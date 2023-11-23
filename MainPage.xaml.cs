5e7b1ebd6dd77ae1c0e00ff117039fc1ed2e8d81
Bayu Hyuuga <radamanthys_ok@yahoo.com>
2023-11-23 17:26:37 +0700

    hello world

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -11,14 +11,15 @@
 
         private void OnCounterClicked(object sender, EventArgs e)
         {
-            count++;
+            //count++;
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+            //if (count == 1)
+            //    CounterBtn.Text = $"Clicked {count} time";
+            //else
+            //    CounterBtn.Text = $"Clicked {count} times";
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            //SemanticScreenReader.Announce(CounterBtn.Text);
+            DisplayAlert("Hello", "This is a Maui Message Dialog!", "OK");
         }
     }
 
