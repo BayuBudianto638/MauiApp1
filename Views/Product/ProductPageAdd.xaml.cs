2ecb90bb08c10f742924b5136f56a36f0991b95c
Bayu Hyuuga <radamanthys_ok@yahoo.com>
2023-11-28 16:43:23 +0700

    perbaikan view page

--- Views/Product/ProductPageAdd.xaml.cs
+++ Views/Product/ProductPageAdd.xaml.cs
@@ -6,4 +6,23 @@ public partial class ProductPageAdd : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void OnSaveClick(object sender, EventArgs e)
+    {
+        await DisplayAlert("Add", "Add Customer", "OK");
+        //var customer = new Customer();
+        //customer.Name = CustomerName.Text;
+
+        //var custAppService = new CustomerAppService();
+
+        //var (isResult, isMsg) = await custAppService.Save(customer);
+        //if (isResult == true)
+        //{
+        //    await DisplayAlert("Sukses", "Sukses", "OK");
+        //}
+        //else
+        //{
+        //    await DisplayAlert("Gagal", "Gagal", "OK");
+        //}
+    }
 }
